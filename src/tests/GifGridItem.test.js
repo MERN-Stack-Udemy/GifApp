788217aa3beccd541bee9189d6773e7efd83a9ac
abdiel_249 <abdielorellana3@gmail.com>
2021-01-27 00:23:04 -0400

    test about GifGridItem

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -4,12 +4,29 @@ import { GifsGridItem } from "../components/GifsGridItem"
 
 describe('Test about GifsGridItem', () => {
   
+  const url = 'https://api.giphy.com/seach?rick+y+morty.jpg';
+  const title = 'rick y morty';
+  const wrapper = shallow(<GifsGridItem  url={url} title={title} />);
+  
   test('test snapshot of GifsGritItem', () => {
-    const url = 'holamundo';
-    const title = 'test demo'
-    // 
-    const wrapper = shallow(<GifsGridItem  url={url} title={title} />);
     expect(wrapper).toMatchSnapshot();
+  })
+  
+  test('check title of <p/> text()', () => {
+    const p_title = wrapper.find('p').text();
+    expect( p_title ).toBe( title );
+  })
+  
+  test('<img/> must contain the attributes: \'alt\' and \'src\'', () => {
+    const img = wrapper.find('img');
+    expect( img.prop('src') ).toBe( url );
+    expect( img.prop('alt') ).toBe( title );
+  })
+  
+  test('<div/> should contains the className: \'animate__bounceIn\' ', () => {
+    const div = wrapper.find('div');
+    const className = div.prop('className');
+    expect( className.includes('animate__bounceIn') ).toBe(true)
   })
   
 
