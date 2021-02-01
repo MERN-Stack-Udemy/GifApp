2c42d952a2443e26a133864fd09c779fd4b9d7b7
abdiel_249 <abdielorellana3@gmail.com>
2021-02-01 19:57:00 -0400

    add tests for custom hook: useFetchGifs

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,4 +1,31 @@
+import { useFetchGifs } from "../../Hooks/useFetchGifs";
+import { renderHook } from '@testing-library/react-hooks'
 
 describe('Test on cunstom hook \'useFetchGifs\'', () => {
+  
+  test('should return initial state ', async () => {
+
+    const { result, waitForNextUpdate } = renderHook ( () => useFetchGifs('One Punch') );
+    const { data, loading } = result.current;
+    
+    await waitForNextUpdate();
+
+    expect( data ).toEqual( [] );
+    expect( loading ).toBe( true );
+
+  });
+
+  test('should to return a images array and loading is false', async () => {
+    
+    const { result, waitForNextUpdate } = renderHook ( () => useFetchGifs('One Punch') );
+    
+    await waitForNextUpdate();
+    
+    const { data, loading } = result.current;
+    expect( data.length ).toEqual( 10 );
+    expect( loading ).toBe( false );
+  
+  });
+  
   
 })
